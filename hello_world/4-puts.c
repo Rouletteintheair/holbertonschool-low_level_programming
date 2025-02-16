f4dad55cb2ea05ae220142e9c973535c9ef31bd1
rouletteintheair <sarahclemnt@gmail.com>
2025-02-16 15:55:53 +0000

    put a # include <stdio.> int main ...

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,7 @@
 #!/bin/bash
-gcc $CFILE -o | printev "Programming is like building a multilingual puzzle"| putchar 
+#include <stdio.h>
+
+int main(void) {
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+} 
