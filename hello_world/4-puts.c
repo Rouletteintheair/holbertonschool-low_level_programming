7e15ae7bd79c90b7367737600f0dc67da412d4d4
rouletteintheair <sarahclemnt@gmail.com>
2025-02-17 10:00:37 +0000

    my first script in betty style

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
-	int main(void) {	puts("Programming is like building a multilingual puzzle");	return 0;	}
+
+/**
+ * main - Entry main
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
