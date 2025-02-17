5d0d98f73eb4f6a986031882879a5a59c55361fa
rouletteintheair <sarahclemnt@gmail.com>
2025-02-17 14:21:50 +0000

    put a script with sizeof

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main-Entry main
+ * Return:Always 0
+ */
+
+int main(void)
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf(Size of an int : sizeof(intType));
+	printf(Size of a float : sizeof(floatType));
+	printf(Size of double : sizeof(doubleType));
+	printf(Size of a char : sizeof(charType));
+	return (0);
+}
+
