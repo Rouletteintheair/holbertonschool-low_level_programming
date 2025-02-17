b8c750641d9ccc2d84c161e54933efc126578018
rouletteintheair <sarahclemnt@gmail.com>
2025-02-17 14:38:22 +0000

    make all sentences by size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,10 +12,11 @@ int main(void)
 	double doubleType;
 	char charType;
 
-	printf(Size of an int : sizeof(intType));
-	printf(Size of a float : sizeof(floatType));
-	printf(Size of double : sizeof(doubleType));
-	printf(Size of a char : sizeof(charType));
+	printf(Size of an int : 4 byte(s) sizeof(intType));
+	printf(Size of a float : 4 byte(s) sizeof(floatType));
+	printf(Size of a long long int: 8 byte(s) sizeof(longlongType));
+	printf(Size of a char : 1 byte(s) sizeof(charType));
+	printf(Size of a long int: 4 byte(s) sizeof(longType));
 	return (0);
 }
 
