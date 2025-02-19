a645f7b31741318993e7b97a6ed992a63637e666
rouletteintheair <sarahclemnt@gmail.com>
2025-02-19 15:25:51 +0000

    make the same script

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main-Enrty main
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+
+	}
+	putchar('\n');
+
+	return (0);
+}
