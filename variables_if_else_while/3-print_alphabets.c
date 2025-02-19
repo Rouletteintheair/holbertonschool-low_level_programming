09fa8786602a0214f50e745981c6e26df46b002c
rouletteintheair <sarahclemnt@gmail.com>
2025-02-19 13:32:45 +0000

    put a new loop

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main-Enrty main
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	char o;
+	char p;
+
+	for (i = 'a'; i <= 'z'; i++);
+	for (o = 'A'; o <= 'Z'; i++);
+	
+	{
+		putchar(i);
+		putchar(o);
+	}
+	putchar('\n');
+
+	return (0);
+}
