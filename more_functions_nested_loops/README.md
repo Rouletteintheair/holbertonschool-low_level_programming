dc054a42ea55ca66887d5ee37872314833b3d88e
rouletteintheair <sarahclemnt@gmail.com>
2025-02-24 13:00:40 +0000

    make a script for print a reverse alphabet

---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+https://www.tutorialspoint.com/cprogramming/c_header_files.htm
+https://www.geeksforgeeks.org/what-is-the-purpose-of-a-function-prototype/
+https://www.youtube.com/watch?v=qMlnFwYdqIw
+https://www.tutorialspoint.com/cprogramming/c_functions.htm
+https://www.youtube.com/watch?v=Z3iGeQ1gIss
+
--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,21 +1,22 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
- * main-Enrty main
- * Return:Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char i;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (i = 'z'; i >= 'a'; i--)
 	{
 		putchar(i);
-
 	}
-	putchar('\n');
+		putchar('\n');
 
 	return (0);
-}:
+}
