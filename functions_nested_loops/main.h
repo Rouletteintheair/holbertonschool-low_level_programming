645b18c4270bc02637e514da2d5afbdb47d025f8
rouletteintheair <sarahclemnt@gmail.com>
2025-02-25 13:04:33 +0000

    make a new declaration

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,13 +1,7 @@
 #ifdef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 int _putchar(char c);/** function for the _putchar */
-int main(void);
 
 
 #endif
