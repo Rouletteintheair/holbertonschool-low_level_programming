aea1bdb4820e6509c8002ef08b22fe7ea645c481
rouletteintheair <sarahclemnt@gmail.com>
2025-02-28 08:56:24 +0000

    make a pointer

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter
+ * @n : variable
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to_98(int *n);/** function that takes a pointer to an int as parameter and updates the value it points to 98 */
+
+
+#endif /* MAIN_H */
