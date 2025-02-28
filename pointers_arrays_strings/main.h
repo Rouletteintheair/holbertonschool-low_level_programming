f920b9dc81deb70349021e0667eef3a1bd83b087
rouletteintheair <sarahclemnt@gmail.com>
2025-02-28 09:52:59 +0000

    make a strl

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,5 +3,5 @@
 
 void reset_to_98(int *n);/** function that takes a pointer to an int as parameter and updates the value it points to 98 */
 void swap_int(int *a, int *b);/**function that swaps the values of two integers */
-
+int _strlen(char *s);
 #endif /* MAIN_H */
