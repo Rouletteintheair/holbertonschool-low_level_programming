7508b977c7f11e7f45a56856d6b3f8ba5e487a7c
rouletteintheair <sarahclemnt@gmail.com>
2025-02-28 12:40:27 +0000

    make my script in the betty style

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -3,15 +3,16 @@
 /**
  * _strlen - Fonction qui retourne la longueur d'une chaîne de caractères
  * @str: La chaîne dont on veut calculer la longueur
- * 
+ *
  * Return: La longueur de la chaîne (sans compter le caractère nul '\0')
  */
 int _strlen(char *s)
 {
-	int longueur = 0;/* Tant qu'on ne rencontre pas le caractère nul */
+	int longueur = 0; /* Tant qu'on ne rencontre pas le caractère nul */
+
 	while (s[longueur] != '\0')
 	{
-		longueur++; /* On incrémente le compteur */ 
+		longueur++; /* On incrémente le compteur */
 	}
-	return(longueur);
+	return (longueur);
 }
