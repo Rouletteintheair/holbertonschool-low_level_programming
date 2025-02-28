8ebc6e6faa10ed1e4b518f9c7f010b9e840f73c2
rouletteintheair <sarahclemnt@gmail.com>
2025-02-28 10:16:47 +0000

    remove some space

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,17 @@
 #include "main.h"
+
 /**
  * _strlen - Fonction qui retourne la longueur d'une chaîne de caractères
  * @str: La chaîne dont on veut calculer la longueur
  * 
  * Return: La longueur de la chaîne (sans compter le caractère nul '\0')
  */
-int _strlen(char *str)
+int _strlen(char *s)
 {
-	int longueur = 0;
-
-	/* Tant qu'on ne rencontre pas le caractère nul */
-	while (str[longueur] != '\0')
-    {
-	    longueur++; /* On incrémente le compteur */
-    }
-
-    return longueur;
+	int longueur = 0;/* Tant qu'on ne rencontre pas le caractère nul */
+	while (s[longueur] != '\0')
+	{
+		longueur++; /* On incrémente le compteur */ 
+	}
+	return(longueur);
 }
