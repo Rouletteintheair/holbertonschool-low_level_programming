e376240f35e2d3bd6dc56a626a66c902274fcce2
rouletteintheair <sarahclemnt@gmail.com>
2025-02-28 09:04:27 +0000

    make swap between pointer

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 
 void reset_to_98(int *n);/** function that takes a pointer to an int as parameter and updates the value it points to 98 */
-
+void swap_int(int *a, int *b);/**function that swaps the values of two integers */
 
 #endif /* MAIN_H */
