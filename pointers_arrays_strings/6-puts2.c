433d6d0aa791f2ff6c5980740c09a0e7a28f2c73
rouletteintheair <sarahclemnt@gmail.com>
2025-03-02 14:40:34 +0000

    make

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,16 +3,16 @@
 
 void puts2(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        if (i % 2 == 0)
-        {
-            write(1, &str[i], 1);  /* Use write to output the character */
-        }
-        i++;
-    }
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			write(1, &str[i], 1);  /* Use write to output the character */
+		}
+		i++;
+	}
 
-    write(1, "\n", 1);  /* Print the newline character */
+	write(1, "\n", 1);  /* Print the newline character */
 }
