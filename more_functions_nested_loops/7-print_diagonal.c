4cd0f5d500d61147d12bc61d52bbbecc67047af0
rouletteintheair <sarahclemnt@gmail.com>
2025-03-02 13:17:51 +0000

    make new script

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line on the terminal.
+ * @n: The number of times the character \ should be printed.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');  /* Print only newline if n is 0 or less */
+	}
+	else
+	{
+		for (i = 0; i < n; i++)  /* Outer loop for each diagonal line */
+		{
+			for (j = 0; j < i; j++)  /* Inner loop for spaces before '\' */
+			{
+				_putchar(' ');  /* Print spaces before the '\' */
+			}
+			_putchar('\\');  /* Print '\' for the diagonal */
+			_putchar('\n');   /* Print newline after each '\' */
+		}
+	}
+}
