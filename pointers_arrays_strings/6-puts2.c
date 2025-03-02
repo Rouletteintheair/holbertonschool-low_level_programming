8ff7a1998513f9634c516a2d1248cbb06c4b2fb7
rouletteintheair <sarahclemnt@gmail.com>
2025-03-02 14:38:19 +0000

    make script

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 
 void puts2(char *str)
 {
-	int i = 0;
-	
-	while (str[i] != '\0')
-	{
-		if (i % 2 == 0)
-		{
-			putchar(str[i]);
-		}
-		i++;
-	}
+    int i = 0;
 
-	putchar('\n');
+    while (str[i] != '\0')
+    {
+        if (i % 2 == 0)
+        {
+            write(1, &str[i], 1);  /* Use write to output the character */
+        }
+        i++;
+    }
+
+    write(1, "\n", 1);  /* Print the newline character */
 }
